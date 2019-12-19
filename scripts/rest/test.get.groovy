0e99f2aa2b935df15c97425c6b892c87fb1e6d91
admin admin <evaladmin@example.com>
2019-12-19 17:00:50 +0000

    User admin wrote content /scripts/rest/test.get.groovy

--- scripts/rest/test.get.groovy
+++ scripts/rest/test.get.groovy
@@ -4,7 +4,7 @@ import org.dom4j.Document;
 SiteItem item = siteItemService.getSiteItem("/site/components/contents${params.site}");
 if(item==null)
     return ["err_code":"404","err_message":"Mentioned page not found"];
-Document document = item.getDom;
+Document document = item.getDom();
 
 
 return document;
